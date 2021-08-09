da17afeec0e75a4431cd898aa36cac7242899ecf
David Behrmann <davidbehrmanntx@gmail.com>
2021-08-09 15:48:47 -0400

    working on Category challenge

--- ElevenNote.Data/Category.cs
+++ ElevenNote.Data/Category.cs
@@ -1,27 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ElevenNote.Data
 {
-    public class Note
+    public class Category
     {
         [Key]
-        public int NoteId { get; set; }
-        [ForeignKey(nameof(CategoryId))]
         public int CategoryId { get; set; }
         [Required]
-        public Guid OwnerId { get; set; }
-        [Required]
-        [Display(Name = "Your Note")]
         public string Title { get; set; }
         [Required]
-
-        public string Content { get; set; }
+        public List<Note> HeldNotes { get; set; }
         [Required]
 
         public DateTimeOffset CreatedUtc { get; set; }
